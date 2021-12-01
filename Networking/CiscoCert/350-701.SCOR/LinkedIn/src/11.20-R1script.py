cb015cadbd19985f75632ab1e35a8d1d5bab5e6d
H.-M. Fred Chen <h.m.chen@ieee.org>
2021-12-01 11:24:40 -0800

    feat: add notes for Python scripts

--- Networking/CiscoCert/350-701.SCOR/LinkedIn/src/11.20-R1script.py
+++ Networking/CiscoCert/350-701.SCOR/LinkedIn/src/11.20-R1script.py
@@ -4,24 +4,23 @@ import getpass
 import sys
 import telnetlib
 
-HOST = '192.168.122.252'
-user = raw_input("Telnet username: ")
+HOST = "192.168.122.252"
+user = input("Telnet username: ")
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
 tn.read_until("Username: ")
-tn.write(user + '\n')
+tn.write(user + "\n")
 if password:
-    tn.read_until('Password: ')
-    tn.write(password + '\n')
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
 
-tn.write('enable\n')
-tn.write('cisco\n')
-tn.write('conf t')
-tn.write('int lo0/0')
-tn.write('ip addr 1.1.1.1 255.255.255.255\n')
-tn.write('end\n')
-tn.write('exit\n')
+tn.write("enable\n")
+tn.write("cisco\n")
+tn.write("conf t")
+tn.write("int lo0/0")
+tn.write("ip addr 1.1.1.1 255.255.255.255\n")
+tn.write("end\n")
+tn.write("exit\n")
 
 print(tn.read_all())
-
